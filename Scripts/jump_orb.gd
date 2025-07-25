be3c65059359c9dbe5a7a38605731581ddc0f07c
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-07-24 18:11:33 -0600

    feat: new jump orb visuals

--- Scripts/jump_orb.gd
+++ Scripts/jump_orb.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
+var moving_up = true
+
+var dist = 0
+
+const VELOCITY = 40
+
 func _ready():
 	pass
 
 func _process(delta):
-	pass
-
+	if moving_up:
+		self.position.y -= delta*VELOCITY
+		dist -= delta*VELOCITY
+	else:
+		self.position.y += delta*VELOCITY
+		dist += delta*VELOCITY
+	
+	if dist > 35:
+		moving_up = true
+	elif dist < -35:
+		moving_up = false
 
 func _on_body_entered(body):
 	if body.name == "player":
