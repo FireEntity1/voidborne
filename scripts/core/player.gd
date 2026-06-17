7eb09c68c8d59a60719a0ac0603e1c9f4d99b5c2
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-06-16 19:28:08 -0600

    feat(physics): improved movement

--- scripts/core/player.gd
+++ scripts/core/player.gd
@@ -1,18 +1,12 @@
 extends CharacterBody2D
 
-var camera_float_pos = Vector2.ZERO
-const SMOOTH_X = 8.0
-const SMOOTH_Y = 4.0
-const VERTICAL_DEADZONE = 32.0
+const SPEED = 900.0
+const JUMP_VELOCITY = 1000.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-var jumped = 0.0
+var jumped = JUMP_VELOCITY
 
 func _ready() -> void:
-	camera_float_pos = global_position
-	$camera.top_level = true
+	pass
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -24,33 +18,37 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -jumped
 		jumped -= delta*20.0
-	if Input.is_action_just_released("jump") or (-0.1 < velocity.y and velocity.y < 0.1):
+	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y = 5
-		jumped = 400.0
+		jumped = JUMP_VELOCITY
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_pressed("left"):
+		$sprite.scale.x = 1
+	elif Input.is_action_just_pressed("right"):
+		$sprite.scale.x = -1
 	
-	camera_smooth(delta)
+	#camera_smooth(delta)
 	move_and_slide()
-	position = position.round()
+	#position = position.round()
 
-func camera_smooth(delta: float):
-	var smooth_speed = 12.0
-	var target = global_position
-	camera_float_pos.x = lerp_avg(camera_float_pos.x, target.x, SMOOTH_X, delta)
-	
-	var diff_y = target.y - camera_float_pos.y
-	if abs(diff_y) > VERTICAL_DEADZONE:
-		var actual_target_y = target.y - (sign(diff_y)*VERTICAL_DEADZONE)
-		camera_float_pos.y = lerp_avg(camera_float_pos.y,actual_target_y,SMOOTH_Y,delta)
-	
-	camera_float_pos = camera_float_pos.lerp(global_position, 1.0 - exp(-smooth_speed * delta))
-	
-	$camera.global_position = camera_float_pos.round()
-
-func lerp_avg(current: float, target: float, speed: float, delta: float):
-	return lerp(current, target, 1.0 - exp(-speed * delta))
+#func camera_smooth(delta: float):
+	#var smooth_speed = 12.0
+	#var target = global_position
+	#camera_float_pos.x = lerp_avg(camera_float_pos.x, target.x, SMOOTH_X, delta)
+	#
+	#var diff_y = target.y - camera_float_pos.y
+	#if abs(diff_y) > VERTICAL_DEADZONE:
+		#var actual_target_y = target.y - (sign(diff_y)*VERTICAL_DEADZONE)
+		#camera_float_pos.y = lerp_avg(camera_float_pos.y,actual_target_y,SMOOTH_Y,delta)
+	#
+	#camera_float_pos = camera_float_pos.lerp(global_position, 1.0 - exp(-smooth_speed * delta))
+	#
+	#$camera.global_position = camera_float_pos.round()
+#
+#func lerp_avg(current: float, target: float, speed: float, delta: float):
+	#return lerp(current, target, 1.0 - exp(-speed * delta))
