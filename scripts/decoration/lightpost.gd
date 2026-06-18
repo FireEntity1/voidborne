e25e3d16fd7eb42a8997bd350e4a10a2c7484097
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-06-17 22:57:18 -0600

    feat(voidnexus): basic world sprites

--- scripts/decoration/lightpost.gd
+++ scripts/decoration/lightpost.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite2D
 
 func _ready() -> void:
-	frame = randi_range(1,6)
-	#play("default")
+	frame = randi_range(0,8)
+	while true:
+		await get_tree().create_timer(2.0).timeout
+		frame = randi_range(0,8)
