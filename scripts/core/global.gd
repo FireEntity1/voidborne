fc2aff4230ac09b963dd100eb86d24b106d4045c
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-06-24 21:25:23 -0600

    feat(ui): fading systems

--- scripts/core/global.gd
+++ scripts/core/global.gd
@@ -6,7 +6,13 @@ var time_scale = 1.0
 
 var can_move = true
 
+var fade = {
+	"active": false,
+	"black": true
+}
+
 var player: CharacterBody2D
+var root: Node2D
 
 func _process(delta: float) -> void:
 	Engine.time_scale = move_toward(Engine.time_scale,time_scale,delta*10.0)
@@ -21,6 +27,10 @@ func screen_vingette(show: bool, time: float = 0.0, radius: float = 0.62):
 	if time > 0.0:
 		await get_tree().create_timer(time, true, false, true).timeout
 		vingette.emit(false, radius)
+
+func fadescreen(active = false,black = true):
+	fade.active = active
+	fade.black = black
 
 func mod_can_move(status: bool = true):
 	can_move = status
--- scripts/core/main.gd
+++ scripts/core/main.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 @onready var vingette = $ui/vingette
+@onready var fade = $ui/fade
 
 func _ready() -> void:
 	$game/player.connect("player_hit",_on_player_hit)
+	Global.root = self
 	Global.connect("vingette",_vingette)
+
+func _process(delta: float) -> void:
+	fade.modulate.a = move_toward(fade.modulate.a, 1.0, delta/2.0) if Global.fade.active else move_toward(fade.modulate.a, 0.0, delta/2.0)
+	fade.color = Color(0,0,0) if Global.fade.black else Color(1,1,1)
 
 func _on_player_hit():
 	await get_tree().create_timer(0.3, true, false, true).timeout
