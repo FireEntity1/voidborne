163ba3c17ec0a584a05b99f5bb5394098e8b3f4e
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-07-17 14:24:38 -0600

    fix: instant transition back to outlands on tower enter

--- scripts/world/tower.gd
+++ scripts/world/tower.gd
@@ -3,6 +3,6 @@ extends Node2D
 func _ready() -> void:
 	$to_outlands/collision.disabled = true
 	$to_outlands.monitoring = false
-	await get_tree().create_timer(4.0)
+	await get_tree().create_timer(4.0).timeout
 	$to_outlands/collision.disabled = false
 	$to_outlands.monitoring = true
