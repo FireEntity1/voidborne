9ce8429a33ad1c2d510269aaf0739ab1e7393b03
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-07-20 09:29:23 -0600

    feat: bright upon switch flip

--- scripts/world/lightswitch.gd
+++ scripts/world/lightswitch.gd
@@ -1,4 +1,7 @@
 extends Area2D
 
+@onready var parent = get_parent()
+
 func hit():
 	$sprite.play()
+	parent.flash()
--- scripts/world/tower.gd
+++ scripts/world/tower.gd
@@ -1,8 +1,29 @@
 extends Node2D
 
+@onready var overlay = $overlay
+
+var switched = false
+var col = 0.2
+
 func _ready() -> void:
 	$to_outlands/collision.disabled = true
 	$to_outlands.monitoring = false
 	await get_tree().create_timer(4.0).timeout
 	$to_outlands/collision.disabled = false
 	$to_outlands.monitoring = true
+
+func _physics_process(delta: float) -> void:
+	col = lerp(col,0.0,delta/8.0)
+	if switched:
+		overlay.texture.gradient.colors = PackedColorArray([
+		Color(1,1,1,col),
+		Color(1,1,1,0)
+	])
+
+func flash():
+	Global.screen_vingette(false)
+	overlay.texture.gradient.colors = PackedColorArray([
+		Color(1,1,1,0.2),
+		Color(1,1,1,0)
+	])
+	switched = true
