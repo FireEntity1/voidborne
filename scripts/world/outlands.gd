ce8825d9d05ec5275b18288aaba574a6bd95d369
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-07-20 22:43:22 -0600

    feat: outlands void growth damage

--- scripts/world/outlands.gd
+++ scripts/world/outlands.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var timer = Timer.new()
+
 func _ready() -> void:
 	Dialogic.connect("signal_event",_on_signal)
 	await get_tree().create_timer(0.1).timeout
@@ -7,6 +9,11 @@ func _ready() -> void:
 		Global.screen_vingette(false)
 	else:
 		Global.screen_vingette(true)
+	add_child(timer)
+	timer.wait_time = 0.1
+	timer.one_shot = false
+	timer.connect("timeout",_col_timeout)
+	timer.start()
 
 func _on_signal(arg):
 	if arg == "open_smog_ridge":
@@ -16,7 +23,11 @@ func _on_signal(arg):
 		await $door_over.animation_finished
 		$door_over.play("default")
 
+func _col_timeout():
+	for body in $underground_cover_hit.get_overlapping_bodies():
+		if body.is_in_group("player"):
+			body.hit(1,true,body.global_position)
 
 func _on_underground_cover_2_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body._on_hit_body_entered(self)
+		body.hit(1,true,body.global_position)
