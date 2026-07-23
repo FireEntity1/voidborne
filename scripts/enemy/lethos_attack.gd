d01cc7094030f10e2f9c036c12aebdfbedea40c5
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-07-22 21:03:36 -0600

    feat: more polished lethos attack

--- scripts/enemy/lethos_attack.gd
+++ scripts/enemy/lethos_attack.gd
@@ -1,17 +1,29 @@
-extends Area2D
+extends CharacterBody2D
 
 var goal: Vector2
 var dir
 
+var attack_strength = 1
+
 const SPEED := 900.0
+
+var done = false
 
 func _physics_process(delta: float) -> void:
 	var distance := global_position.distance_to(goal)
-
+	$particles.global_rotation_degrees = 0
 	if distance <= SPEED * delta:
 		global_position = goal
-		queue_free()
-		return
+		if done:
+			return
+		else:
+			done = true
+			$sprite.hide()
+			$collision.disabled = true
+			$particles.emitting = true
+			await get_tree().create_timer(1.5).timeout
+			queue_free()
+			return
 
 	rotation = global_position.angle_to_point(goal) - PI / 2
 	global_position = global_position.move_toward(goal, SPEED * delta)
