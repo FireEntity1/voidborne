895bdbd9c1588c6484f08cb89a702032db1baefe
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-07-27 20:25:57 -0600

    feat: pause menu

---
+++ scripts/core/pause.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+
+var paused = false
+var transitioning = false
+
+
+func _ready() -> void:
+	hide()
+
+func _physics_process(delta: float) -> void:
+	if not transitioning:
+		return
+	if paused:
+		$bg.modulate.a = lerp($bg.modulate.a,1.0,delta*10)
+	else:
+		$bg.modulate.a = lerp($bg.modulate.a,0.0,delta*5)
+	$bg/center/vbox/resume.modulate.a = $bg.modulate.a
+	$bg/center/vbox/settings.modulate.a = $bg.modulate.a
+	$bg/center/vbox/quit.modulate.a = $bg.modulate.a
+	if $bg.modulate.a >= 1.0:
+		transitioning = false
+	if $bg.modulate.a <= 0.0:
+		transitioning = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+		get_viewport().set_input_as_handled()
+		
+func toggle_pause() -> void:
+	transitioning = true
+	paused = not get_tree().paused
+	get_tree().paused = paused
+	
+	if paused:
+		show()
+	else:
+		hide()
+
+func _on_resume_button_up() -> void:
+	toggle_pause()
+
+func _on_quit_button_up() -> void:
+	# TS TEMPORARY CHANGE IT TO ACTUAL TITLE MENU WHEN U MAKE IT
+	get_tree().change_scene_to_file("res://scenes/main.tscn") 
