313c386fc52bb96a8c68b7c2ce12f17aaf37b3ee
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-07-30 22:27:59 -0600

    feat: better squishers

--- scripts/world/squisher.gd
+++ scripts/world/squisher.gd
@@ -14,14 +14,28 @@ func _physics_process(delta: float) -> void:
 	if location == "up":
 		$collision.position.y = move_toward($collision.position.y,$top.position.y,delta*48.0)
 	else:
-		$collision.position.y = move_toward($collision.position.y,$bottom.position.y,delta*80.0)
+		#$collision.position.y = move_toward($collision.position.y,$bottom.position.y,delta*80.0)
+		pass
 
 func _on_timer_timeout() -> void:
 	if location == "up":
 		location = "down"
+		$timer.wait_time = 0.5
+		$timer.start()
+		slam()
 	else:
 		location = "up"
+		$timer.start()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 			body.hit(1,true,global_position)
+
+func slam():
+	var tween = create_tween()
+	tween.tween_property(
+		$collision,
+		"position:y",
+		$bottom.position.y,
+		0.5
+	).set_trans(Tween.TRANS_EXPO)
