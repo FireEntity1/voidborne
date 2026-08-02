f8f46b4cc04cca0b3100514d68ae10457985aba7
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-08-02 13:09:02 -0600

    feat: working forsaken prism death

--- scripts/enemy/top_bomb.gd
+++ scripts/enemy/top_bomb.gd
@@ -9,19 +9,33 @@ var player_pos = Vector2.ZERO
 const gravity = 5000
 var falling = false
 
+var dead = false
+
 func _ready() -> void:
 	$darken.start()
 	$sprite.material = $sprite.material.duplicate()
 
 func _physics_process(delta: float) -> void:
+	if dead:
+		return
 	if falling:
 		velocity.y += gravity*delta
 		$sprite.material.set_shader_parameter("dir",velocity/5000.0)
-		velocity.x = move_toward(velocity.x,position.x-player_pos.x,delta)
+		#velocity.x = move_toward(velocity.x,position.x-player_pos.x,delta)
 	else:
 		$sprite.material.set_shader_parameter("dir",Vector2.ZERO)
-	global_position.y += velocity.y*delta
-	if is_on_floor_only():
+	#global_position.y += velocity.y*delta
+	move_and_slide()
+
+	for i in get_slide_collision_count():
+		var body = get_slide_collision(i).get_collider()
+		if body.is_in_group("player"):
+			body.hit(attack_strength,true,global_position)
+			die()
+			return
+
+	if is_on_floor():
+		$fall.play()
 		die()
 	if darkening:
 		$sprite.self_modulate -= Color(delta, delta, delta)/8.0
@@ -44,10 +58,14 @@ func _on_darken_timeout() -> void:
 	darkening = not darkening
 
 func damage(amtd):
-	$collision.disabled = true
-	queue_free()
+	die()
 
 func die():
+	if dead:
+		return
+	dead = true
 	$collision.disabled = true
-	$sprite.queue_free()
+	$sprite.hide()
 	$death_particles.emitting = true
+	await $death_particles.finished
+	queue_free()
